91218b7f6215daa0a88f55c319f99421886ba6d6
liyahjanelle <80090323+liyahjanelle@users.noreply.github.com>
2021-03-05 04:02:52 -0800

    Update and rename README.md to index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+	<title> Hope </title>
+</head>
+<body bgcolor="sky blue">
+<h1 align="center">
+Hope Despite The Pandemic
+</h1>
+<p> <img src="hope.jpg" height="200px" width="200px"
+<left>
+<p> <i>
+Explanation:The relevance of our design to the theme HOPE, is about to cherish a desire with anticipation. Our drawing depicts that we are stronger together and we have confidence. It’s also like holding the hands of your friend through thick and thin. When we are together, we are giving hopes to other people also our self. Being one with the other people means giving confidence and hope to each other.
+<p> <b>
+Message for those who are affected by the pandemic
+<p> <i>
+Xyenith: to those who are afected by the pandemic don't lose hope because after this God has better plan for all of us. Just say his name and if its in our faith that we will be safe.
+<p> <i>
+Ella: A message for those who are affected by the covid 19 we should be stay safe,stay at home and also pray to the Lord have faith in him and have hope and love and we should take care of ourself and those people around us.
+<p> <i>
+Joeylla: To our dear frontliners i just want to thank you for all your sacrifices even your work is risky your still here to help us and to people your dedications and skills are making difference.You're courage and selfishness are humbling. You all done too much for our country so don't stop and lose hope.Thank you and big salute to all of you.
+<p> <i>
+Jireh:To all the frontliners who risk their life in the middle of the pandemic.We truly give our gratitude for that.We have gone through so much here in the quarantine days and their were people who lost their jobs and there are some who can't sustain the needs of their family. Let us all not worry and let's just continue to hope for the best of our country and to the whole world to overcome it all.
+<p> <i>
+Glezel: To all people who are affected in this pandemic which is covid 19.I hope everyone is safe ay may God guide and protect all of you and those who are suffering right now we all that we can overcome it and face a new life.Maybe you are now suffering but i hope and pray that this pandemic will end soon.
+<p> <i>
+Aliya:For the people that feel lonely and is in the verge of giving up in this pandemic that we are currently experiencing.I would like to remind you that there will be always hope in our life. We should always remeber our love one's like family,friends and especially God. We should always remember not to give up since God has a plan for us even if you don't see it yet.Just remember that he is always there with us and don't wipe off the smile plastered in your face.
+
+</left>
+</body>
+
+</html>
+
+
